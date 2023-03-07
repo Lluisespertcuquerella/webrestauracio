f03f9b1e7785a750ac6701ee21ec235e232e615b
Superlewis2023 <espertcuquerellalluis@gmail.com>
2023-03-07 10:45:11 +0100

    Vamos mejorando la parte visual, para ello hacemos la parte principal del menú, o al menos estructurándolo!

--- vista/inicio.php
+++ vista/inicio.php
@@ -2,15 +2,29 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/config.css">
     </head>
         <body>
-            <?php
-                echo 'Hola mundo';
-            ?>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-8">
+                        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+                            <div class="container fluid">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Inicio</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="col-lg-2"></div>
+                </div>
+            </div>
         </body>
 </html>
 
