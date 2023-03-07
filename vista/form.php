a9e6448332e10f644bc449d3683941229cc0d5e7
Superlewis2023 <espertcuquerellalluis@gmail.com>
2023-03-07 14:20:37 +0100

    Implementando primer cambio de la interfaz del menú. Versión 0.1

--- vista/form.php
+++ vista/form.php
@@ -6,24 +6,26 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" type="text/css" href="../css/form.css">
+        <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+
     </head>
-    <body>
+    <body class="fondoFormulari">
         <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-2"></div>
-                    <div class="col-lg-8">
-                        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+                    <div class="col-lg-4"></div>
+                    <div class="col-lg-6">
+                        <nav class="navbar navbar-expand-sm fondoFormulari navbar-dark">
                             <div class="container fluid">
                                 <ul class="navbar-nav">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="../index.php">Inicio</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="../index.php">Inicio</a>
                                     </li>
-                                        <a class="nav-link" href="#">¿Quienes somos?</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="#">¿Quienes somos?</a>
                                     <li>
                                     </li>
-                                        <a class="nav-link" href="#">¿A que nos dedicamos?</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="#">¿A que nos dedicamos?</a>
                                     <li>
-                                        <a class="nav-link" href="#">¡Nuestros trabajos!</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="#">¡Nuestros trabajos!</a>
                                     </li>
                                 </ul>
                             </div>
@@ -35,10 +37,10 @@
         <br>
         <br>
         <br>
-        <div class="row">
+        <div class="row fondoFormulari">
             <div class="col-lg-3"></div>
-            <div class="col-lg-6 fondoFormulari">
-                <form method="POST" class="fondoFormulari">
+            <div class="col-lg-6">
+                <form method="POST">
                     <div class="form-group">
                         <label for="nombre"><p class="textoBotones">Nombre:</p></label>
                         <input type="text" class="form-control" name="nombre">
--- vista/inicio.php
+++ vista/inicio.php
@@ -6,33 +6,34 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <link rel="stylesheet" type="text/css" href="css/form.css">
     </head>
-        <body>
+        <body class="fondoFormulari">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-2"></div>
-                    <div class="col-lg-8">
-                        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+                    <div class="col-lg-3"></div>
+                    <div class="col-lg-7">
+                        <nav class="navbar navbar-expand-sm navbar-dark">
                             <div class="container fluid">
                                 <ul class="navbar-nav">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">Inicio</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="#">Inicio</a>
                                     </li>
-                                        <a class="nav-link" href="#">¿Quienes somos?</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="#">¿Quienes somos?</a>
                                     <li>
                                     </li>
-                                        <a class="nav-link" href="#">¿A que nos dedicamos?</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="#">¿A que nos dedicamos?</a>
                                     <li>
-                                        <a class="nav-link" href="#">¡Nuestros trabajos!</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="#">¡Nuestros trabajos!</a>
                                     </li>
                                     <li>
-                                        <a class="nav-link" href="vista/form.php">¡Contacta con nosotros!</a>
+                                        <a class="nav-link colorLetras botonRedondo" href="vista/form.php">¡Contacta con nosotros!</a>
                                     </li>
                                 </ul>
                             </div>
                         </nav>
                     </div>
-                    <div class="col-lg-2"></div>
+                    <div class="col-lg-3"></div>
                 </div>
             </div>
         </body>
